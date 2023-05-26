d64d1c8eab390455eb9c9f6c66c1b543c3f4b68e
Miguel Arenas <migue.arenas91@gmail.com>
2023-05-25 20:38:27 -0500

    connection with fetch and show content done

---
+++ js/main.js
@@ -0,0 +1,24 @@
+console.log(`hello beautiful world!`);
+
+// HTML elements
+const adviceIdDiv = document.getElementById('adviceId')
+const adviceContent = document.getElementById('adviceContent')
+
+async function generateAdvice() {
+  try {
+
+  const response = await fetch('https://api.adviceslip.com/advice')
+
+  if (!response.ok) {
+      throw new Error(`Error request`)
+      }
+  const data = await response.json()
+  const {id: adviceId, advice } = data.slip
+  
+  adviceIdDiv.innerText = adviceId
+  adviceContent.innerText = `"${advice}"`
+
+  } catch (error) {
+  console.error(`Error:`, error)
+  }
+}
